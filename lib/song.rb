3fcb6535bec417ad309955d9756af21c430f4fd1
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-19 01:14:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,10 @@ class Song
 
   attr_accessor :name, :artist, :genre
 
+  @@count = []
+  @@artists = []
+  @@genres = []
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
