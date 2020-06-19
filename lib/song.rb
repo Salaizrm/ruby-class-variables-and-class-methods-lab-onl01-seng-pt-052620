af1dc63613d1b4ef55c36db5f794dcef48eb6e5e
Robert Salaiz <work.salaizrm@gmail.com>
2020-06-19 01:25:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,12 +28,20 @@ class Song
   end
 
   def self.genre_count
-    @@genres[genre].count
+    genre_count = {}
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count = 1
+      end
+    end
+    genre_count
   end
 
-  # def self.artist_count
-  #   @@artists.count
-  # end
+  def self.artist_count
+    @@artists.count
+  end
 
 
 end
